4c4fc301843dbbdddaa87583b75d803565d09b64
Mehmet <63320170+memte@users.noreply.github.com>
2022-09-05 12:51:03 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,6 +1,6 @@
 const { Permissions, MessageEmbed } = require("discord.js");
 const { readdirSync } = require("fs");
-const { owner } = require("../../config.js");
+const { owner } = require("../config.js");
 const commandFiles = readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
  module.exports = {
