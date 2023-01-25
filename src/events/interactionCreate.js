f0a8f1e21782ba0093e6723cc7957be3e3493388
Mehmet <63320170+memte@users.noreply.github.com>
2023-01-25 19:13:25 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,11 +1,8 @@
-const { Permissions, MessageEmbed } = require("discord.js");
-const { readdirSync } = require("fs");
-
  module.exports = {
 	name: 'interactionCreate',
 	execute: async(interaction) => {
-  let client = interaction.client;
-    	if (!interaction.isCommand()) return;
+         let client = interaction.client;
+    	 if (!interaction.isCommand()) return;
    	 if(interaction.user.bot) return;
          const command = client.commands.get(interaction.commandName)
          command.run(client, interaction)
