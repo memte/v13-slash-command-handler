bd39610fb25d967a5ecd6ad2d3d57dec0dafa98e
Mehmet <63320170+memte@users.noreply.github.com>
2023-08-20 01:37:07 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,8 @@
 export default {
-	name: 'ready',
-	once: true,
-	execute(client) {
-    let activities = [ `Developed by memte.`, `${client.user.username}` ], i = 0;
-    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: "LISTENING" }), 120000);
+ name: 'ready',
+ once: true,
+ execute(client) {
+  let activities = [ `Developed by memte.`, `${client.user.username}` ], i = 0;
+  setInterval(() => client.user.presence.set({ activities: [{name: `${activities[i++ % activities.length]}`, type: "LISTENING" }]}), 120000);
+  client.log(`${client.user.username} Aktif Edildi!`)
 }};
