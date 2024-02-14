0c24bf152632c30abf5316c2537c2773ca016fe5
Mehmet <63320170+memte@users.noreply.github.com>
2024-02-14 20:18:56 +0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Collection, Intents } = require("discord.js");
+const { Client, Intents, Partials } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING, Intents.FLAGS.MESSAGE_CONTENT, Intents.FLAGS.GUILD_SCHEDULED_EVENTS, Intents.FLAGS.AUTO_MODERATION_CONFIGURATION, Intents.FLAGS.AUTO_MODERATION_EXECUTION],
   partials: ["USER", "CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "GUILD_SCHEDULED_EVENT"],
@@ -6,75 +6,12 @@ const client = new Client({
 });
 const config = require("./src/config.js");
 const { readdirSync } = require("node:fs")
-const moment = require("moment");
 
 let token = config.token
 
-client.commands = new Collection()
-client.commandAliases = new Collection()
-client.slashCommands = new Collection()
-client.slashDatas = []
-
-function log(message) {
-  console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${message}`);
-};
-client.log = log
-
-// Command Handler
-const commandFolders = readdirSync("./src/commands");
-
-Promise.all(commandFolders.map(async (category) => {
-  const commandFiles = await readdirSync(`./src/commands/${category}`);
-
-  await Promise.all(commandFiles.map(async (file) => {
-    const commands = await require(`./src/commands/${category}/${file}`);
-
-    if (commands) {
-      if (commands.prefix) {
-        // Prefix Command
-        const prefixCommand = commands.prefix;
-        client.commands.set(prefixCommand.name, prefixCommand);
-
-        if (prefixCommand.aliases && Array.isArray(prefixCommand.aliases)) {
-          prefixCommand.aliases.forEach(alias => {
-            client.commandAliases.set(alias, prefixCommand.name);
-          });
-        }
-      }
-
-      if (commands.slash) {
-        // Slash Command
-        const slashCommand = commands.slash;
-        client.slashDatas.push(slashCommand.data.toJSON());
-        client.slashCommands.set(slashCommand.data.name, slashCommand);
-      }
-    }
-  }));
-}));
-
-// Event Handler
-const eventFiles = readdirSync("./src/events");
-
-Promise.all(eventFiles.map(async (file) => {
-const event = await require(`./src/events/${file}`);
-
-if (event.once) {
-  client.once(event.name, (...args) => event.execute(...args));
-} else {
-  client.on(event.name, (...args) => event.execute(...args));
-}
-}));
-
-// Process Listeners
-process.on("unhandledRejection", e => { 
-   console.log(e)
- }) 
-process.on("uncaughtException", e => { 
-   console.log(e)
- })  
-process.on("uncaughtExceptionMonitor", e => { 
-   console.log(e)
- })
-//
+readdirSync("./src/utils").map(async (file) => {
+  const util = await require(`./src/utils/${file}`);
+  util.execute(client);
+});
 
 client.login(token)
---
+++ src/utils/functions.js
@@ -0,0 +1,7 @@
+const moment = require("moment");
+module.exports = {
+    execute: async (client) => {
+        client.log = function log(message) {
+            console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${message}`);
+          };
+    }}
