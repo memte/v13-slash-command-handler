a2a840e9a787a910bfdb19b4a7fd2000ab4158a0
Mehmet <63320170+memte@users.noreply.github.com>
2024-04-26 21:03:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const { Client, Intents, Partials } = require("discord.js");
+const {Client, Intents} = require('discord.js');
 const client = new Client({
-  intents: Object.values(Intents.FLAGS), 
-  partials: ["USER", "CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "GUILD_SCHEDULED_EVENT"],
-  shards: "auto"
+	intents: Object.values(Intents.FLAGS),
+	partials: ['USER', 'CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION', 'GUILD_SCHEDULED_EVENT'],
+	shards: 'auto',
 });
-const config = require("./src/config.js");
-const { readdirSync } = require("node:fs")
+const config = require('./src/config.js');
+const {readdirSync} = require('node:fs');
 
-let token = config.token
+const {token} = config;
 
-readdirSync("./src/utils").map(async (file) => {
-  const util = await require(`./src/utils/${file}`);
-  util.execute(client);
+readdirSync('./src/utils').map(async file => {
+	const util = await require(`./src/utils/${file}`);
+	util.execute(client);
 });
 
-client.login(token)
+client.login(token);
