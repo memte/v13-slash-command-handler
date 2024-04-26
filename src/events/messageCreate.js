a8b9be97054e71d97372f9e6df2b4619754f64ed
Mehmet <63320170+memte@users.noreply.github.com>
2024-04-26 21:05:09 +0300

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,34 +1,49 @@
-import config from "../config.js"
-import { Collection } from "discord.js";
-import ms from "ms";
-const cooldown = new Collection()
+import config from '../config.js';
+import {Collection} from 'discord.js';
+const cooldown = new Collection();
 
 export default {
- name: 'messageCreate',
- execute: async(message) => {
-  let client = message.client;
-  if (message.author.bot) return;
-  if (message.channel.type === 'DM') return;
-  let prefix = config.prefix
-  if(!message.content.startsWith(prefix)) return;
-  const args = message.content.slice(prefix.length).trim().split(/ +/g); 
-  const cmd = args.shift().toLowerCase();
-  if(cmd.length == 0 ) return;
-  let command = client.commands.get(cmd)
-  if(!command) command = client.commands.get(client.commandAliases.get(cmd));
+	name: 'messageCreate',
+	async execute(message) {
+		const {client} = message;
+		if (message.author.bot) {
+			return;
+		}
 
-  if(command) {
-    if(command.cooldown) {
-      const nowDate = message.createdTimestamp
-      const waitedDate = new Date(nowDate + (cooldown.get(`${command.name}-${message.author.id}`) - Date.now())).getTime()
-      if(cooldown.has(`${command.name}-${message.author.id}`)) return message.reply({ content: `Cooldown şuan aktif lütfen <t:${Math.floor(waitedDate / 1000)}:R> tekrar deneyin.`}).then(msg => setTimeout(() => msg.delete(), cooldown.get(`${command.name}-${message.author.id}`) - Date.now()))
-      command.prefixRun(client, message, args)
-      cooldown.set(`${command.name}-${message.author.id}`, Date.now() + command.cooldown)
-      setTimeout(() => {
-        cooldown.delete(`${command.name}-${message.author.id}`)
-      }, command.cooldown);
-  } else {
-    command.prefixRun(client, message, args)
-  }
-  }
-  }};
+		if (message.channel.type === 'DM') {
+			return;
+		}
+
+		const {prefix} = config;
+		if (!message.content.startsWith(prefix)) {
+			return;
+		}
+
+		const args = message.content.slice(prefix.length).trim().split(/ +/g);
+		const cmd = args.shift().toLowerCase();
+		if (cmd.length === 0) {
+			return;
+		}
+
+		let command = client.commands.get(cmd);
+		command ||= client.commands.get(client.commandAliases.get(cmd));
+
+		if (command) {
+			if (command.cooldown) {
+				const nowDate = message.createdTimestamp;
+				const waitedDate = new Date(nowDate + (cooldown.get(`${command.name}-${message.author.id}`) - Date.now())).getTime();
+				if (cooldown.has(`${command.name}-${message.author.id}`)) {
+					return message.reply({content: `Cooldown şuan aktif lütfen <t:${Math.floor(waitedDate / 1000)}:R> tekrar deneyin.`}).then(msg => setTimeout(() => msg.delete(), cooldown.get(`${command.name}-${message.author.id}`) - Date.now()));
+				}
+
+				command.prefixRun(client, message, args);
+				cooldown.set(`${command.name}-${message.author.id}`, Date.now() + command.cooldown);
+				setTimeout(() => {
+					cooldown.delete(`${command.name}-${message.author.id}`);
+				}, command.cooldown);
+			} else {
+				command.prefixRun(client, message, args);
+			}
+		}
+	},
+};
