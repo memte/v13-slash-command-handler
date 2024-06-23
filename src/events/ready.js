7279e389b2a9397415704cef650fc6c8670fbb1e
Mehmet <63320170+memte@users.noreply.github.com>
2024-06-24 02:40:29 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,14 +6,11 @@ module.exports = {
 	once: true,
 	async execute(client) {
 		const rest = new REST({version: '9'}).setToken(client.token);
-		const activities = ['Developed by memte.', `${client.user.username}`];
-		let nowActivity = 0;
-		function botPresence() {
-			client.user.presence.set({activities: [{name: `${activities[nowActivity++ % activities.length]}`, type: 'LISTENING'}]});
-			setTimeout(botPresence, 300000);
-		}
 
-		botPresence();
+		client.user.presence.set({
+			activities: [{name: 'Developed by memte.', type: 'LISTENING'}],
+		});
+
 		client.log(`${client.user.username} Aktif Edildi!`);
 		//
 		try {
